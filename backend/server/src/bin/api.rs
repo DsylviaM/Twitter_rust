8b1e4f91c6d95db325f6aaa126b01cb9a3d14270
DsylviaM <98478500+DsylviaM@users.noreply.github.com>
2024-02-15 15:43:06 +0300

    beckend/bin/api_cli_20

--- backend/server/src/bin/api.rs
+++ backend/server/src/bin/api.rs
@@ -2,10 +2,12 @@ use std::net::SocketAddr;
 use color_eyre::{Result, Help, eyre::Context};
 
 use clap::{command, Parser, Subcommand};
+use dotenvy::dotenv;
+use tracing::{debug, info};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
-struct Cli {
+struct cli {
     // api -d <URL>
     // api --database-url
     #[clap(
@@ -33,6 +35,34 @@ enum Command {
 }
 
 async fn run() -> Result<()> {
+    color_eyre::install()?;
+
+    let use_dotenv = dotenvy::dotenv();
+
+    let args = cli::parse();
+
+    uchat_server::logging::setup(args.verbosity);
+
+    if let Ok(path) = use_dotenv {
+        debug!(target: "uchat_server", dot_env_found = true, path = %path.to_string_lossy());
+    } else {
+        debug!(target: "uchat_server", dot_env_found = false);
+    }
+
+    if let Some(command) = args.command {
+        match command {
+            Command::GenKey => {
+                let mut rng = uchat_crypto::new_rng();
+                info!(target: "uchat_server", "generating privat key...");
+                let (key, _) = uchat_server::cli::gen_keys(&mut rng)?;
+                let path = "private_key.base64";
+                std::fs::write(path, key.as_str())?;
+                info!(target: "uchat_server", path=path, "privat key saved to disk");
+                info!(target: "uchat_server", path=path, "set API_PRIVATE_KEY environment variable with the content of the key in order to use it");
+                return Ok(());
+            }
+        }
+    }
     Ok(())
 }
 
