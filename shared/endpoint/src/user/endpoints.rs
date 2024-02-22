5397f94a8255636ac06ede30ece53a68a0de578d
DsylviaM <98478500+DsylviaM@users.noreply.github.com>
2024-02-22 13:48:51 +0300

    endpoint_28

---
+++ shared/endpoint/src/user/endpoints.rs
@@ -0,0 +1,18 @@
+use  uchat_domain::{ids::*,Password, Username};
+
+use crate::Endpoint;
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct CreateUser {
+    pub username: Username,
+    pub password: Password,
+}
+
+impl Endpoint for CreateUser {
+    const URL: &'static str = "/account/create";
+}
+
+pub struct CreateUserOk {
+    pub user_id: User_Id,
+    pub username:Username,
+}
