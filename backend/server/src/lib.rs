134e691a4cb91bc74332ec409de8fab376d59a87
DsylviaM <98478500+DsylviaM@users.noreply.github.com>
2024-02-22 17:26:20 +0300

    data_nase_connection_extractor_30

--- backend/server/src/lib.rs
+++ backend/server/src/lib.rs
@@ -5,6 +5,7 @@ use uchat_query::{AsyncConnection, AsyncConnectionPool, QueryError};
 pub mod logging;
 pub mod router;
 pub mod error;
+pub mod extractor;
 
 #[derive(FromRef, Clone)]
 pub struct  AppState{
