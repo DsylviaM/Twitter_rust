b8817dfb7a30c83c8b6a00170a110d61bad2c8ba
DsylviaM <98478500+DsylviaM@users.noreply.github.com>
2024-03-05 14:35:03 +0300

    macros_newid_37

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -12,7 +12,7 @@ pub fn App(cx: Scope) -> Element {
     cx.render (rsx! {
         Router {
              Route{ to: page::ACCOUNT_REGISTER, page::Register {} },
-             Route{ to: page::ACCOUNT_LOGIN, page::login {} },
+             Route{ to: page::ACCOUNT_LOGIN, page::Login {} },
         }
     })
 }
