087df632f561a78f5b4cbe6ca7f7d9a18ab4dbd2
DsylviaM <98478500+DsylviaM@users.noreply.github.com>
2024-04-01 11:27:34 +0300

    navbar_buttons_47

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,11 +1,12 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+use crate::{elements::Navbar, prelude::*};
 
 use dioxus_router::{Route, Router};
 use fermi::use_init_atom_root;
 
-use crate::prelude::*;
+
 
 pub fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
@@ -14,6 +15,7 @@ pub fn App(cx: Scope) -> Element {
              Route{ to: page::ACCOUNT_REGISTER, page::Register {} },
              Route{ to: page::ACCOUNT_LOGIN, page::Login {} },
              Route{ to: page::HOME, page::Home {} },
+             Navbar{}
         }
     })
 }
