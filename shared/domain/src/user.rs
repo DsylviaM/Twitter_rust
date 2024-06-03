b37a450885227ae0df7fff8bc136c5c9eea085be
DsylviaM <98478500+DsylviaM@users.noreply.github.com>
2024-06-03 11:24:31 +0300

    imail_domain_127

--- shared/domain/src/user.rs
+++ shared/domain/src/user.rs
@@ -1,13 +1,18 @@
 use nutype::nutype;
+use once_cell::sync::OnceCell;
+use regex::Regex;
+
+use serde::{Deserialize, Serialize};
 
 use crate::UserFacingError;
 
+// NOTE Using the `nutype` crate to easily add field validation
 #[nutype(
     validate(not_empty, len_char_min = 3, len_char_max = 30),
     derive(Debug, Clone, AsRef, Serialize, Deserialize, PartialEq)
 )]
 pub struct Username(String);
-
+// NOTE The nutype validate macro automatically generates the UsernameError enum
 impl UserFacingError for UsernameError{
     fn formatted_error(&self) -> &'static str {
         match self {
@@ -54,4 +59,45 @@ impl UserFacingError for DisplayNameError{
         }
     }
 }
+
+static EMAIL_REGEX: OnceCell<EmailRegex> = OnceCell::new();
+
+#[derive(Debug)]
+pub struct EmailRegex(Regex);
+
+impl EmailRegex {
+    pub fn global() -> &'static Self {
+        EMAIL_REGEX.get().expect("email regex is not initialized")
+    }
+
+    pub fn init() -> Self {
+        Self(regex::Regex::new(r#"^\S+@\S+\.\S{1,64}$"#).unwrap())
+    }
+
+    pub fn is_valid<T: AsRef<str>>(&self, email: T) -> bool {
+        self.0.is_match(email.as_ref())
+    }
+}
+
+fn is_valid_email(email: &str) -> bool {
+    let email_regex = EMAIL_REGEX.get_or_init(EmailRegex::init);
+
+    email_regex.is_valid(email)
+}
+
+#[nutype(
+    validate(predicate = is_valid_email),
+    derive(AsRef, Clone, Debug, Serialize, Deserialize, PartialEq)
+)]
+pub struct Email(String);
+
+// NOTE The nutype validate macro automatically generates the EmailError enum
+impl UserFacingError for EmailError {
+    fn formatted_error(&self) -> &'static str {
+        match self {
+            Self::PredicateViolated => "Email is not valid. Format: your_name@example.com",
+        }
+    }
+}
+
 
