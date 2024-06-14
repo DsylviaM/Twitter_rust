60374e263d421de2888a9841b56cfd1608be979e
DsylviaM <98478500+DsylviaM@users.noreply.github.com>
2024-06-14 12:38:20 +0300

    sidebar_buttons_148

--- frontend/src/elements/sidebar.rs
+++ frontend/src/elements/sidebar.rs
@@ -4,6 +4,8 @@ use crate::prelude::*;
 use dioxus:: prelude::*;
 use fermi::{use_atom_ref, UseAtomRef};
 
+use super::local_profile;
+
 pub fn use_sidebar(cx: &ScopeState) -> &UseAtomRef<SidebarManager> {
     use_atom_ref(cx, crate::app::SIDEBAR)
 }
@@ -30,6 +32,8 @@ impl SidebarManager {
 pub fn Sidebar(cx: Scope) -> Element {
 
     let sidebar = use_sidebar(cx);
+    let router = use_router(cx);
+    let local_profile = use_local_profile(cx);
 
     let sidebar_width = if sidebar.read().is_open() {
         "w-[var(--sidebar-width)]"
@@ -56,7 +60,28 @@ pub fn Sidebar(cx: Scope) -> Element {
             class: "{sidebar_width} z-[100] fixed top-0 left-0 h-full
             overflow-x-hidden
             flex flex-col
-            navbar-bg-color transition-[width] duration-300"
+            navbar-bg-color transition-[width] duration-300",
+            a {
+                class: "sidebar-navlink border-t",
+                onclick: move |_| {
+                    sidebar.write().close();
+                    router.navigate_to(page::PROFILE_EDIT);
+                },
+                "Edit Profile"
+            }
+            a {
+                class: "sidebar-navlink border-t",
+                onclick: move |_| {
+                    use chrono::Utc;
+                    use uchat_domain::ids::SessionId;
+                    crate::util::cookie::set_session("".to_string(), SessionId::new(), Utc::now());
+                    local_profile.write().user_id = None;
+                    local_profile.write().image = None;
+                    sidebar.write().close();
+                    router.navigate_to(page::ACCOUNT_LOGIN);
+                },
+                "Logout"
+            }
         }
     })
 }
